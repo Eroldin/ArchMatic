80583159e085a4e233c4cbc655402b41b05969bb
Eroldin <54679426+Eroldin@users.noreply.github.com>
2020-09-18 12:43:13 +0200

    Update preinstall.sh
    Created an additional HOME partition

--- preinstall.sh
+++ preinstall.sh
@@ -8,12 +8,12 @@
 #-------------------------------------------------------------------------
 
 echo "-------------------------------------------------"
-echo "Setting up mirrors for optimal download - US Only"
+echo "Setting up mirrors for optimal download - NL Only"
 echo "-------------------------------------------------"
 timedatectl set-ntp true
 pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+curl -s "https://www.archlinux.org/mirrorlist/?country=NL&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
 
 
@@ -31,26 +31,30 @@ echo -e "\nFormatting disk...\n$HR"
 echo "--------------------------------------"
 
 # disk prep
-sgdisk -Z ${DISK} # zap all on disk
+# sgdisk -Z ${DISK} # zap all on disk
 sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
 # create partitions
 sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
-sgdisk -n 2:0:0     ${DISK} # partition 2 (Root), default start, remaining
+sgdisk -n 2:0:+100000M ${DISK} # partition 2 (Root), default start, 50GB
+sgdisk -n 3:0:0 ${DISK} # partition 3 (Home), default start, remaining
 
 # set partition types
 sgdisk -t 1:ef00 ${DISK}
 sgdisk -t 2:8300 ${DISK}
+sgdisk -t 3:8300 ${DISK}
 
 # label partitions
 sgdisk -c 1:"UEFISYS" ${DISK}
 sgdisk -c 2:"ROOT" ${DISK}
+sgdisk -c 3:"HOME" ${DISK}
 
 # make filesystems
 echo -e "\nCreating Filesystems...\n$HR"
 
 mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
 mkfs.ext4 -L "ROOT" "${DISK}2"
+mkfs.ext4 -L "HOME" "${DISK}3"
 
 # mount target
 mkdir /mnt
@@ -58,6 +62,8 @@ mount -t ext4 "${DISK}2" /mnt
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
 mount -t vfat "${DISK}1" /mnt/boot/
+mkdir /mnt/home
+mount -t ext4 "${DISK}3" /mnt/home
 
 echo "--------------------------------------"
 echo "-- Arch Install on Main Drive       --"
