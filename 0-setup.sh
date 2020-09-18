f61343f6fad3a5bd3203e535e8fa7b5867fec064
Eroldin <54679426+Eroldin@users.noreply.github.com>
2020-09-18 13:16:28 +0200

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -48,7 +48,7 @@ sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 timedatectl --no-ask-password set-timezone Europe/Amsterdam
 timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG=en_US.UTF-8 LC_COLLATE= LC_ADDRESS=nl_NL.UTF-8 LC_IDENTIFICATION=nl_NL.UTF-8 LC_MEASUREMENT=nl_NL.UTF-8 LC_MONETARY=nl_NL.UTF-8 LC_NAME=nl_NL.UTF-8 LC_NUMERIC=nl_NL.UTF-8 LC_PAPER=nl_NL.UTF-8 LC_TELEPHONE=nl_NL.UTF-8 LC_TIME=nl_NL.UTF-8
+localectl --no-ask-password set-locale LANG=en_US.UTF-8 LC_ADDRESS=nl_NL.UTF-8 LC_IDENTIFICATION=nl_NL.UTF-8 LC_MEASUREMENT=nl_NL.UTF-8 LC_MONETARY=nl_NL.UTF-8 LC_NAME=nl_NL.UTF-8 LC_NUMERIC=nl_NL.UTF-8 LC_PAPER=nl_NL.UTF-8 LC_TELEPHONE=nl_NL.UTF-8 LC_TIME=nl_NL.UTF-8
 
 
 # Set keymaps
