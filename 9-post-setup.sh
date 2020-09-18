67186db7887c6bc4c14131762b7bc156d06a7e8b
Eroldin <54679426+Eroldin@users.noreply.github.com>
2020-09-18 12:25:42 +0200

    Update 9-post-setup.sh
    Changed setxkbmap to US Intl

--- 9-post-setup.sh
+++ 9-post-setup.sh
@@ -31,7 +31,7 @@ xsetroot -solid darkgrey
 #xrdb -merge $HOME/.Xresources
 
 # Caps to Ctrl, no caps
-setxkbmap -layout us -option ctrl:nocaps
+setxkbmap -layout us -variant intl
 if [ -d /etc/X11/xinit/xinitrc.d ] ; then
     for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
         [ -x "\$f" ] && . "\$f"
